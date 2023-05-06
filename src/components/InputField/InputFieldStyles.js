ec690bda175d490a4ed61c602a7cebd2d84caadf
Mkhue <mkhue47@gmail.com>
2023-05-06 23:35:52 +0200

    first commit

--- src/components/InputField/InputFieldStyles.js
+++ src/components/InputField/InputFieldStyles.js
@@ -1,38 +1,65 @@
 import styled, { css } from "styled-components";
-import { secondaryColor } from "../CssVariables/Index";
+import {
+  mainThemeColor,
+  secondaryThemeColor,
+  whiteColor,
+} from "../../Css/Variables";
 
 export const InputContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   position: relative;
-  width:100%;
-  .label-password-icon{
+  height: fit-content;
+  width: ${(props) => (props.inputSize === "large" ? "100%" : "auto")};
+  .input-icon {
     position: absolute;
     right: 10px;
   }
 `;
 
 export const Input = styled.input`
-  width: 100%;
-  height: 50px;
+  height: ${(props) =>
+    props.inputSize === "large"
+      ? "35px"
+      : props.inputSize === "medium"
+      ? "30px"
+      : "25px"};
+  width: ${(props) =>
+    props.inputSize === "large"
+      ? "auto"
+      : props.inputSize === "medium"
+      ? "250px"
+      : props.inputSize === "small"
+      ? "180px"
+      : "130px"};
   border-radius: 7px;
   font-style: normal;
   font-weight: 400;
   font-size: 15px;
   line-height: 30px;
   letter-spacing: 0.26px;
-  color: #555555;
-  background: none;
+  color: ${secondaryThemeColor};
   outline: none;
-  margin: 10px 0px 10px 0px;
-  border: 1px solid #cecece;
+  border: ${(props) =>
+    props.hasFloatingLabel
+      ? `1px solid ${secondaryThemeColor}`
+      : `1px solid ${secondaryThemeColor}`};
   padding-left: 10px;
-  &:hover {
-    border: 1px solid ${secondaryColor};
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
   }
-  &:focus {
-    border: 1px solid ${secondaryColor};
+  ::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  &:hover {
+    border: ${(props) =>
+      props.hasFloatingLabel
+        ? `1px solid ${mainThemeColor}`
+        : `1px solid ${secondaryThemeColor}`};
+    //background-color: ${(props) => props.backgroundColor};
   }
 `;
 export const InputLabel = styled.p`
@@ -40,18 +67,17 @@ export const InputLabel = styled.p`
   position: absolute;
   left: 10px;
   padding: 0px 3px 0px 3px;
-  color: #cecece;
+  color: ${secondaryThemeColor};
   pointer-events: none;
+  background-color: ${whiteColor};
   transition: all 200ms ease-in-out;
   ${(props) =>
     props.hovered &&
     css`
-      color: ${secondaryColor};
+      color: ${mainThemeColor};
       transform: translateY(-20px);
       z-index: 2;
-      background-color: white;
+      background-color: ${whiteColor};
       padding: 0px 2px 0px 2px;
     `}
 `;
-
-
